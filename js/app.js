f447441b5a3501de52099008b9358dc57298e929
achhunna <achhunna@gmail.com>
2016-06-21 21:04:03 -0700

    Trivia app

---
+++ js/app.js
@@ -0,0 +1,100 @@
+var trivia = angular.module("trivia", []);
+
+trivia.controller("screenController", function($scope, $http, $timeout, $interval){
+
+	var url = "js/qna.json";
+	var counter = 0;
+	var stopWatch;
+	$scope.triviaData = [];
+	$scope.response = {};
+
+	$http.get(url)
+		.success(function(data){
+			$scope.data = data;
+			//$scope.triviaData = data[Math.floor(Math.random()*data.length)];
+		})
+		.error(function(err){
+			console.log(err);
+		});
+
+	//Start Timer
+	$scope.reset = function(){
+		counter = 20;
+	}
+	$scope.start = function(){
+
+		$scope.response = {};
+
+		stopWatch = $interval(function(){
+			$scope.response = {
+				message: counter,
+				symbol: "",
+				class: "",
+			};
+			if(counter === 0){
+				$scope.stop();
+				$timeout(function(){
+					$scope.response = {};
+				}, 3000)
+				$scope.response = {
+					message: "Time",
+					symbol: "!",
+					class: "wrong",
+				};
+			}
+			counter -= 1;
+		},1000);
+	};
+
+	//Stop Timer
+	$scope.stop = function(){
+		if(angular.isDefined(stopWatch)){
+			$interval.cancel(stopWatch);
+			stopWatch = undefined;
+		}
+	};
+
+	$scope.retrieveQ = function(category){
+		//console.log(category);
+		var loopBreak = false;
+		$scope.reset();
+		$scope.start();
+
+		angular.forEach($scope.data, function(v, k){
+			if(!loopBreak){
+				if(v["category"] === category){
+					$scope.triviaData = v;
+					$scope.data.splice(k, 1);
+					//prevId = $scope.triviaData["id"];
+					loopBreak = true;
+				}
+			}
+		});
+
+	};
+
+	$scope.checkAnswer = function(answer, option){
+		$scope.stop();
+		if(answer === option){
+			$timeout(function(){
+				$scope.response = {
+						message: "Right",
+						symbol: "!",
+						class: "right",
+				};
+			}, 500)
+			$scope.response = {};
+		}else{
+			$timeout(function(){
+				$scope.response = {
+						message: "Wrong",
+						symbol: "",
+						class: "wrong",
+				};
+			}, 500)
+			$scope.response = {};
+		}
+	};
+
+
+});
